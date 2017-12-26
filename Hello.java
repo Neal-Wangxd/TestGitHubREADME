6ff3c08aea5353e7897fef2147d8f82b4ccbabfb
Neal-Wangxd <wangxd_work@163.com>
2017-12-26 10:25:25 +0800

    修改了字符编码

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello World");
-		System.out.println("当前时间为:"+new java.util.Date().getTime());
+		System.out.println("获取当前时间:"+new java.util.Date().getTime());
 	}
 }
