43d69afd6e12eba7eaec3f8266c2005b9e7a2c92
Neal-Wangxd <wangxd_work@163.com>
2017-12-26 13:58:22 +0800

    this is a new test branch file

--- Branch.java
+++ Branch.java
@@ -2,5 +2,6 @@ public class Branch {
     public static void main(String[] args) {
 		System.out.println("BRANCH");
         System.out.println("这是一个测试自分支的测试类");
+		System.out.println("这是一个测试子分支的测试类");
     }
 }
