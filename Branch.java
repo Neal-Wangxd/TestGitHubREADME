503b3261d3519d1d1906ea6d52f97d27f83b5d8f
Neal-Wangxd <wangxd_work@163.com>
2017-12-26 13:54:09 +0800

    test branch

--- Branch.java
+++ Branch.java
@@ -1,9 +1,6 @@
 public class Branch {
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
 		System.out.println("BRANCH");
->>>>>>> brh
         System.out.println("这是一个测试自分支的测试类");
     }
 }
