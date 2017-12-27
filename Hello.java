824a381357ff3e27c802a4e4af125faa3839bc77
Neal-Wangxd <wangxd_work@163.com>
2017-12-27 10:23:30 +0800

    wxd change

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello World");
 		System.out.println("获取当前时间:"+new java.util.Date().getTime());
+		System.out.println("Hello 这是一个测试暂存的分支");
 	}
 }
